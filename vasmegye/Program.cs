4a9302d77148ef439403805b447cc2a7f804d4d0
TiborNorbert <levaitibor4@gmail.com>
2022-10-10 09:08:23 +0200

    Add project files.

---
+++ vasmegye/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace vasmegye
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<double> adat = new List<double>();
+            Console.WriteLine("2.feladat: Adatok beolvasása, tárolása");
+            Console.WriteLine("4.feladat: Ellenőrzés");
+            Console.WriteLine($"5.feladat: Vas megyében a vizsgált évek alatt{adat.Count}csecsemő született.");
+            Console.WriteLine("6.feladat: Fiúk száma:{}");
+            Console.WriteLine("\nProgram vége.");
+            Console.ReadKey();
+        }
+    }
+}
